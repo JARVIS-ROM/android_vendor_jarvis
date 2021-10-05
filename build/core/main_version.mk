f06d5741b00ce7ad961ee4b41f84d3a2a92d3f88
Michael Bestas <mkbestas@lineageos.org>
2021-10-05 23:37:55 +0300

    build: Replace obsolete ADDITIONAL_BUILD_PROPERTIES
    Fixes:
    error: ADDITIONAL_BUILD_PROPERTIES is obsolete.  Please use ADDITIONAL_SYSTEM_PROPERTIES.

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,16 +1,16 @@
 # Build fingerprint
 ifneq ($(BUILD_FINGERPRINT),)
-ADDITIONAL_BUILD_PROPERTIES += \
+ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 endif
 
 # JARVIS System Version
-ADDITIONAL_BUILD_PROPERTIES += \
+ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.jarvis.version=$(JARVIS_VERSION) \
     ro.jarvis.releasetype=$(JARVIS_BUILDTYPE) \
     ro.jarvis.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
     ro.modversion=$(JARVIS_VERSION)
 
 # JARVIS Platform Display Version
-ADDITIONAL_BUILD_PROPERTIES += \
+ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.jarvis.display.version=$(JARVIS_DISPLAY_VERSION)
