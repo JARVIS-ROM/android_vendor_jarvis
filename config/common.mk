5e246c27d2b487bc0686380f7fd3a2705e79992e
Ali Hasan <ahb7671@gmail.com>
2021-10-22 16:13:59 +0500

    vendor: compile updater
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -80,6 +80,10 @@ PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 # Disable vendor restrictions
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
+# JARVIS packages
+PRODUCT_PACKAGES += \
+    Updater
+
 # Filesystems tools
 PRODUCT_PACKAGES += \
     fsck.ntfs \
